c67a59fefe9c11c687a0d0bef556e8a41ceb2988
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-20 09:34:43 +0300

    add student controller actions and routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,21 @@
 class StudentsController < ApplicationController
+    def create
+        student = Student.create(student_params)
+        if student.valid?
+            session[:student_id] = student.id
+            render json: student, status: :created
+        else
+            render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: @current_student
+    end
+
+    private
+
+    def student_params
+        params.permit(:name, :email, :password, :password_confirmation, :phone_no)
+    end
 end
