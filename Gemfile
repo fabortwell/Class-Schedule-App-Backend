37bbcaddc598e970ff6933e5f38c984037adf445
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-22 09:26:57 +0300

    seed student data

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "2.7.4"
 
+#Use faker gem for fake data
+gem 'faker', '~> 1.6', '>= 1.6.6'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Seeding data..."
+ 10.times do 
+    Student.create(name: faker.name, email: faker.internet.email(), phone_no: faker.phone.number('07-##-###-###'))
+ end
+puts "Done seeding."
