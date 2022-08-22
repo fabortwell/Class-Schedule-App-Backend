ba4d396502283cc0777885c129f6a14110661b1b
user <tre@Brian.local>
2022-08-22 11:05:23 +0300

    added comment model and associtaed controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,10 @@
+class CommentsController < ApplicationController
+   def create
+    @comment = Comment.new(comment_params)
+   end
+
+   private
+   def comment_params
+    params.require(:comment).permit(:student_id)
+   end
+end
