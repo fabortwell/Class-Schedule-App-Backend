dc9e40a4a715ecbdc22d9c5ef8d47e27d15b369d
user <tre@Brian.local>
2022-08-22 11:31:31 +0300

    added migration file for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :student
-    
+    belongs_to :schedule
 end
---
+++ db/migrate/20220822082649_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :content
+      t.belongs_to :student, null: false, foreign_key: true
+      t.belongs_to :schedule, null: false, foreign_key: true
+      t.integer :likes
+
+      
+      t.timestamps
+    end
+  end
+end
