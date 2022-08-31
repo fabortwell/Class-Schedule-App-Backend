073d6cc3e0a7837ca81bf0aedecd71c99366105d
Justine Kenyansa <kenyansajustine@gmail.com>
2022-08-31 12:52:21 +0300

    create swagger files and update comments serializer

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -13,7 +13,6 @@ RSpec.configure do |config|
   # be generated at the provided relative path under swagger_root
   # By default, the operations defined in spec files are added to the first
   # document below. You can override this behavior by adding a swagger_doc tag to the
-  # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
